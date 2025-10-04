c5bb357c6dd08d3f245effa1392eb9c7b524090c
juan <juanmproducoes@gmail.com>
2025-10-04 13:11:37 -0300

    ok

--- prisma/migrations/20251004144843_init/migration.sql
+++ prisma/migrations/20251004144843_init/migration.sql
@@ -1,4 +1,24 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GlobalSetting" (
+    "id" INTEGER NOT NULL,
+    "backgroundUrl" TEXT,
+
+    CONSTRAINT "GlobalSetting_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Destination" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
@@ -16,6 +36,9 @@ CREATE TABLE "Destination" (
 
     CONSTRAINT "Destination_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- AddForeignKey
 ALTER TABLE "Destination" ADD CONSTRAINT "Destination_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;

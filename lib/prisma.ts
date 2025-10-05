7bb674db6aca9e8cb80df753825585587dd1b4c7
juan <juanmproducoes@gmail.com>
2025-10-04 21:31:19 -0300

    2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 
-type PrismaGlobal = { prisma?: PrismaClient };
+type PrismaGlobal = { prisma?: Prisma.DefaultPrismaClient };
 
 const globalForPrisma = globalThis as typeof globalThis & PrismaGlobal;
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma: Prisma.DefaultPrismaClient =
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
